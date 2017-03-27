35ddbfe87504911920f57fa0df085b142ed5cfb4
dd <dd@u16.12>
2017-03-27 23:36:27 +0200

    stuff is back to App

--- App.js
+++ App.js
@@ -14,38 +14,59 @@ import Modal from "./components/modal";
 import SliderTop from "./components/slider-top";
 import MainNav from "./components/nav";
 import AllPics from "./components/allpics";
+import Blog from "./components/blog";
+import Article from "./components/article";
 // css
 require ('./production/css/newstyle.css');
 
 class App extends React.Component {
   constructor() {
     super();
-		this.state = {
-			components : [
-        // <HeaderLarge key="0"/>,
-        // <MainNav key="1" activeLink={this.openActiveLink.bind(this)}/>,
-        // <Slider allPics={this.showAllPics.bind(this)} images={imgsObj.imgs} key="4" />,
-        // <Footer key="6"/>,
-			]
-		}
-  }
-  openActiveLink(rerender) {
-    this.setState({components:[
-      // <HeaderLarge key="0"/>,
-      // <MainNav key="1" activeLink={this.openActiveLink.bind(this)} />,
-      // <Footer key="6"/>,
-    ]})
   }
 
-  showAllPics() {
-    console.log('kuku');
-  }
+  cb = (zz) => {
+    console.log(zz);
+  };
 
+  showAllPics = (pics) => {
+    console.log(pics);
+    if (pics)
+    console.log(pics);
+    renderAllPics(pics)
+  };
 
+  renderSlider = () => {
+    return (
+      <Slider showPics={ this.showAllPics.bind(this) } />
+    )
+  };
+
+  renderAllPics = (pics) => {
+    return (
+      <AllPics pics={ pics } showAllPics={ this.showAllPics.bind(this) } />
+    )
+  };
 
   render() {
 
+    return (
+      <Router>
+        <div>
 
+          <Route path="/" component={HeaderLarge}/>
+          <Route path="/" component={MainNav}/>
+          <Route exact={true} path="/" component={SliderTop}/>
+          <Route exact={true} path="/" component={Manifesto}/>
+          <Route exact={true} path="/" component={CTA}/>
+          <Route exact={true} path="/" render={this.renderSlider.bind(this)}/>
+          <Route exact={true} path="/" component={Blog}/>
+          <Route path="/blog" component={Blog}/>
+          <Route path="/fotos" render={this.renderAllPics.bind(this)}/>
+          <Route path="/" component={Footer}/>
+
+        </div>
+      </Router>
+    )
   }
 }
 
--- index.js
+++ index.js
@@ -5,66 +5,11 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Link, Match, Miss, Switch } from 'react-router-dom';
 // css
 require ('./production/css/newstyle.css');
-// components
+
 import App from "./App";
-import HeaderLarge from "./components/header";
-import Manifesto from "./components/manifesto";
-import Footer from "./components/footer";
-import Slider from "./components/slider_component";
-import CTA from "./components/cta";
-import Modal from "./components/modal";
-import SliderTop from "./components/slider-top";
-import MainNav from "./components/nav";
-import AllPics from "./components/allpics";
-import Blog from "./components/blog";
-import Article from "./components/article";
-
-
-const cb = (zz) => {
-  console.log(zz);
-}
-
-const renderCTA = () => {
-  return (
-    <CTA callback={cb}/>
-  )
-}
-
-const showAllPics = (pics) => {
-  console.log(pics);
-  if (pics)
-    console.log(pics);
-  // renderAllPics(pics)
-}
-
-const renderSlider = () => {
-  return (
-    <Slider showPics={ showAllPics } />
-  )
-}
-
-const renderAllPics = (pics) => {
-  return (
-    <AllPics pics={ pics } showAllPics={ showAllPics } />
-  )
-}
 
 render ((
 
-<Router>
-  <div>
+    <App />
 
-    <Route path="/" component={HeaderLarge}/>
-    <Route path="/" component={MainNav}/>
-    <Route exact={true} path="/" component={SliderTop}/>
-    <Route exact={true} path="/" component={Manifesto}/>
-    <Route exact={true} path="/" render={renderCTA}/>
-    <Route exact={true} path="/" render={renderSlider}/>
-    <Route exact={true} path="/" component={Blog}/>
-    <Route path="/blog" component={Blog}/>
-    <Route path="/fotos" render={renderAllPics}/>
-    <Route path="/" component={Footer}/>
-
-  </div>
-</Router>
 ), document.getElementById("zz"));
